a27684391203e2faa8176c615517fbc438a04768
xobek <xobekme@gmail.com>
2024-02-16 16:28:22 +0100

    Update Window.h

--- Soft/src/Soft/Window.h
+++ Soft/src/Soft/Window.h
@@ -13,7 +13,7 @@ namespace Soft {
 		uint32_t Width;
 		uint32_t Height;
 
-		WindowProps(const std::string& title = "Hazel Engine",
+		WindowProps(const std::string& title = "Soft Engine",
 			uint32_t width = 1600,
 			uint32_t height = 900)
 			: Title(title), Width(width), Height(height)
